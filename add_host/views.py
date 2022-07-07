64e8b593143dc21bdf4c1779131a2b9051516784
dmitry <demonekr@yandex.ru>
2022-07-07 12:19:43 +0300

    create main app

---
+++ add_host/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render  
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import redirect
+
+    
+def signup(request):
+    header = 'Регистрация пользователя'
+    form = UserCreationForm(request.POST or None) 
+    if form.is_valid():  
+        form.save()
+        return redirect('index')
+
+    context = {  
+        'form': form,
+        'header': header
+    }
+    return render(request, 'register.html', context)  
